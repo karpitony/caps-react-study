349766aae40827292e21e0d39a3428a24bf73101
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-08-20 14:31:32 +0900

    6주차 - 4 - vercel.json, 버셀 분석기

--- react-markdown-blog/src/layouts/RootLayout.tsx
+++ react-markdown-blog/src/layouts/RootLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout() {
   return (
     <div>
       <Header />
       <Outlet />
+      <Analytics />
     </div>
   )
 }

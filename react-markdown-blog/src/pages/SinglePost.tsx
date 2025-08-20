e7f1cfb65978c24e45e97d2ef3fd33b4f70bb9d9
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-08-20 14:13:10 +0900

    6주차 - 3

--- react-markdown-blog/src/pages/SinglePost.tsx
+++ react-markdown-blog/src/pages/SinglePost.tsx
@@ -9,17 +9,12 @@ export default function SinglePostPage( ) {
 
   if (loading) 
     return <div>loading...</div>
-
   if (error)
     return <div>error</div>
   return (
     <div>
       <p>포스트: {postId}</p>
-      <div className="markdown-body">
-        {/* ReactMarkdown 컴포넌트로 마크다운 텍스트 렌더링 */}
-        <ReactMarkdown>{data}</ReactMarkdown>
-      </div>
+      <ReactMarkdown>{data}</ReactMarkdown>
     </div>
   )
-
 }
